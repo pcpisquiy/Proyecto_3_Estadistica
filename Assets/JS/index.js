0b86307ff73cac5f536ad2b7454cb95dd9809cf2
pcpisquiy <pcpisquiy@gmail.com>
2022-02-25 22:18:16 -0600

    creacion del navbar y del scroller

---
+++ Assets/JS/index.js
@@ -0,0 +1,6 @@
+const arrow = document.querySelector('#arrow');
+const scroll = VanillaScrollspy(arrow);
+const navbar = document.querySelector('#Menu');
+const scrollspy = VanillaScrollspy(navbar);
+scroll.init();
+scrollspy.init();
