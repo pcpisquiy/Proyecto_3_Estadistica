0b94b1dcfc9e48f24c9e46d485c90432a48f5f6f
pcpisquiy <pcpisquiy@gmail.com>
2022-02-27 21:59:47 -0600

    finalizacion

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,6 +1,33 @@
-const arrow = document.querySelector('#arrow');
-const scroll = VanillaScrollspy(arrow);
-const navbar = document.querySelector('#Menu');
+const navbar = document.querySelector('#navbar');
 const scrollspy = VanillaScrollspy(navbar);
-scroll.init();
+const Arrow = document.querySelector('#arrow');
+const scrollArrow = VanillaScrollspy(Arrow);
 scrollspy.init();
+scrollArrow.init();
+function disableScroll(){  
+    var x = window.scrollX;
+    var y = window.scrollY;
+    window.onscroll = function(){ window.scrollTo(x, y) };
+}
+function enableScroll(){  
+    window.onscroll = null;
+}
+/*===== MENU SHOW Y HIDDEN =====*/ 
+const navMenu = document.getElementById('nav__menu'),
+      openMenu = document.getElementById('nav__menu--open'),
+      closeMenu = document.getElementById('nav__menu--close');
+
+/*SHOW*/ 
+openMenu.addEventListener('click', (e)=>{
+    navMenu.classList.toggle('show');
+    window.addEventListener('scroll', disableScroll());
+
+})
+
+/*HIDDEN*/
+closeMenu.addEventListener('click', ()=>{
+    navMenu.classList.remove('show');
+    window.addEventListener('scroll', enableScroll());
+
+})
+AOS.init();
